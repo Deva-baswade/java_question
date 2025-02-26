5b925d57393d73180e41f164de35d0190374fac0
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-26 17:40:44 +0530

    this some question on basics

--- problem1.java
+++ problem1.java
@@ -22,6 +22,55 @@ public class problem1 {
         }
 
 
+        //palidrome ckeck
+       String name = "ded";
+       int num = name.length();
+       boolean isPalidrome = true;
+       for(int i=0;i<num/2;i++){
+        if(name.charAt(i)!=name.charAt(num-i-1)){
+            isPalidrome=false;
+            break;
+        }
+           
+       }
+       if(isPalidrome){
+        System.out.println("string is the palidrome");
+       }else{
+        System.out.println("string is not palidrome");
+       }
+
+      //sum of digit =>123 = 1+2+3
+
+      int mum = sc.nextInt();
+      int sum=0;
+      while(mum!=0){
+        int digit = mum%10;
+        sum=sum+digit;
+        mum/=10;
+      }System.out.println(sum);
+
+      //largest in array
+
+    int[] nums = {1,2,3,4,5};
+    int largest = nums[0];
+    for(int i=0;i<nums.length;i++){
+        if(largest<nums[i]){
+            largest=nums[i];
+        }
+    }System.out.println(largest);
+
+
+
+
+    
+
+  
+
+
+
+
+
+
         
     }
 }

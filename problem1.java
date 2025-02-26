bd3af178af6f19b073836e7e89ff500fd874390e
Baswade_Devanand_Shivajirao <baswadedeva45@gmail.com>
2025-02-26 15:49:30 +0530

    is str is empty

---
+++ problem1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //to check the string is emprty or not
+        String str=" ";
+        if(str.isEmpty()){
+            System.out.println(" string is empty");
+        }else{
+            System.out.println(" string is not empty");
+        }
+
+
+        String str1 = "deav";
+        int length = str1.length();
+        if(length==0){
+            System.out.println("str is empty");
+
+        }else{
+            System.out.println("is not empty");
+        }
+
+
+        
+    }
+}
